2f9a4b3485ff7f6c9a0be2ac2baf7e1bedb9a40c
Vakirtzis Anastasios <anastasiosvakirtzis@gmail.com>
2020-05-01 13:58:38 +0300

    Added .env file to hide MongoDB Atlas URI. Added .env-sample to show how it should work.

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from "express";
+import mongoose from "mongoose";
+import { resolvers } from "./resolvers";
+import { typeDefs } from "./typeDefs";
+require("dotenv").config();
+
+const startServer = async () => {
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  server.applyMiddleware({ app });
+
+  const MONGODB_URI = process.env.MONGO_URI;
+
+  await mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  console.log("database connected");
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+};
+
+startServer();
